530e342ba352c0625eb4ad1e7a5cefb599a9cd45
Vlad Pana <vladpana02@gmail.com>
2023-10-05 19:30:49 +0300

    2nd commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const root = document.getElementById('root');
+const rootElement = ReactDOM.createRoot(root);
+rootElement.render(<App />);
